bdf6aa5a0777c49caa1708e02ebb799616aa1f2b
Raghav Dadhich <raghavvinayak24@gmail.com>
2023-02-03 13:34:54 +0530

    requirements

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 from flask import Flask, Response, request,jsonify, make_response
 from flask_restful import Api
 from routes.route import initialize_routes
-from decouple import config
 
 app=Flask(__name__)
 
